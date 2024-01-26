8c839b14445d146e0d7fcadf3da84e709a8dcb7b
YoungKameSennin <shaowei045@gmail.com>
2024-01-25 20:26:35 -0800

    Problem 2 Solved by fetching subject's name

--- Codes/BeLog.py
+++ Codes/BeLog.py
@@ -1,9 +1,8 @@
-# BeLog
-# version 1.0
-# Data: Nov 12, 2023
+# BeLog Server
+# version 1.1
+# Data: Jan 25, 2024
 import keyboard
 import subprocess
-import pyautogui
 import time
 
 subject_mapping = {
@@ -19,28 +18,49 @@ subject_mapping = {
     'ctrl+alt+9': 9
 }
 logger_path = "C:/Data/Log.txt"
-application_path = 'C:\\BeLog\\dist\\Recording_Control.exe'
+application_path = 'C:/BeLog/dist/Recording_Control.exe'
+projects_folder = "C:/Data/Projects"
+
 rec = 0
 subject = 0
 log_time = time.strftime("%T-%m/%d/%Y") + ": "
 print(log_time + "Ready")
 with open(logger_path, 'a') as file:
     print(log_time + "Ready", file=file)
+
+
 while True:
     if not rec:
         for key_combination, value in subject_mapping.items():
             if keyboard.is_pressed(key_combination):
                 time.sleep(0.15)
                 subject = value
+
+                project_file_name = projects_folder + \
+                    "/subject" + str(subject) + ".boris"
+
+                # open the project file
+                with open(project_file_name, 'r') as file:
+                    content = file.read()
+                    # get the start index of the subject's name and its length
+                    name_start = content.find("\"name\": \"") + 9
+                    name_end = content[name_start:].find("\",")
+                # save the subject's name
+                subject_name = content[name_start: name_start + name_end]
+                file.close()
+
+                # print current subject information
                 log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                print(log_time + "Changed to Subject" + str(subject))
+                print(log_time + "Changed to Subject" +
+                      str(subject) + ": " + subject_name)
                 with open(logger_path, 'a') as file:
-                    print(log_time + "Changed to Subject" + str(subject), file=file)
+                    print(log_time + "Changed to Subject" +
+                          str(subject) + ":" + subject_name, file=file)
     if not rec and keyboard.is_pressed('ctrl+alt+r'):
-        subprocess.Popen([application_path, str(subject)])
+        subprocess.Popen([application_path, str(subject), str(subject_name)])
         rec = 1
         time.sleep(0.15)
-    
+
     if rec and keyboard.is_pressed('ctrl+alt+r'):
         time.sleep(0.15)
         rec = 0
--- Codes/Recording_Control.py
+++ Codes/Recording_Control.py
@@ -1,6 +1,6 @@
 # Recording Control
-# version 1.0
-# Data: Nov 12, 2023
+# version 1.1
+# Data: Jan 25, 2024
 
 import pyrealsense2 as rs
 import numpy as np
@@ -42,6 +42,7 @@ def time_converter(time):
     formatted_duration = f"{hours:02d}:{minutes:02d}:{seconds:02d}:{milliseconds:03d}"
     return formatted_duration
 
+
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
@@ -50,14 +51,15 @@ config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 recordings_folder = "C:/Data/Recordings"
 projects_folder = "C:/Data/Projects"
 logger_path = "C:/Data/Log.txt"
-    
+
 color_path = ''
 depth_path = ''
 colorwriter = None
 depthwriter = None
 start_time = 0
 
-b1_start, b1_stop, b2_start, b2_stop, b3_start, b3_stop, b4_start, b4_stop = (-1 for i in range(8))
+b1_start, b1_stop, b2_start, b2_stop, b3_start, b3_stop, b4_start, b4_stop = (
+    -1 for i in range(8))
 b1, b2, b3, b4 = (0 for i in range(4))
 
 pipeline.start(config)
@@ -70,7 +72,8 @@ log_time = time.strftime("%T-%m/%d/%Y") + ": "
 
 subject = 0
 if len(sys.argv) > 1:
-        subject = sys.argv[1]
+    subject = sys.argv[1]
+    subject_name = sys.argv[2]
 
 try:
     while True:
@@ -79,32 +82,40 @@ try:
         color_frame = frames.get_color_frame()
         if not depth_frame or not color_frame:
             continue
-        
+
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
+            depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
         if recording:
             if colorwriter is None or depthwriter is None:
                 current_time = time.strftime("%Y-%m-%d%a%H-%M-%S")
                 current_time_prj = time.strftime("%Y-%m-%d%a%T")
-                current_time_prj = current_time_prj[:11] + current_time_prj[13:]
+                current_time_prj = current_time_prj[:11] + \
+                    current_time_prj[13:]
 
-                color_path = os.path.join(recordings_folder, f'{current_time}_rgb.avi')
-                depth_path = os.path.join(recordings_folder, f'{current_time}_depth.avi')
-                audio_path = os.path.join(recordings_folder, f'{current_time}_audio.wav')
-                
-                colorwriter = cv2.VideoWriter(color_path, cv2.VideoWriter_fourcc(*'XVID'), 30, (1280, 720), 1)
-                depthwriter = cv2.VideoWriter(depth_path, cv2.VideoWriter_fourcc(*'XVID'), 30, (1280, 720), 1)
+                color_path = os.path.join(
+                    recordings_folder, f'{current_time}_rgb.avi')
+                depth_path = os.path.join(
+                    recordings_folder, f'{current_time}_depth.avi')
+                audio_path = os.path.join(
+                    recordings_folder, f'{current_time}_audio.wav')
+
+                colorwriter = cv2.VideoWriter(
+                    color_path, cv2.VideoWriter_fourcc(*'XVID'), 30, (1280, 720), 1)
+                depthwriter = cv2.VideoWriter(
+                    depth_path, cv2.VideoWriter_fourcc(*'XVID'), 30, (1280, 720), 1)
                 start_time = time.time()  # Capture start time
             # Capture audio frames
             audio_data = audio_stream.read(audio_chunk_size)
             audio_frames.append(audio_data)
-            
+
             colorwriter.write(color_image)
             depthwriter.write(depth_colormap)
-        
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
+            depth_image, alpha=0.03), cv2.COLORMAP_JET)
         combined_image = np.vstack((color_image, depth_colormap))
 
         # Resize the combined image to fit the window size
@@ -112,18 +123,19 @@ try:
 
         # Display the combined and resized image in the "Preview" window
         cv2.imshow('Preview', resized_combined_image)
-        
+
         key = cv2.waitKey(1)
-        
 
         if not recording:
             recording = True
             events_list = []
             log_time = time.strftime("%T-%m/%d/%Y") + ": "
-            print(log_time + "Recording started. Current subject: Subject" + str(subject))
+            print(log_time + "Recording started. Subject" +
+                  str(subject) + ": " + subject_name)
             with open(logger_path, 'a') as file:
-                print(log_time + "Recording started. Current subject: Subject" + str(subject), file=file)
-        
+                print(log_time + "Recording started. Subject" +
+                      str(subject) + ": " + subject_name, file=file)
+
         if keyboard.is_pressed('ctrl+alt+r'):
             recording = False
             if start_time:
@@ -131,12 +143,14 @@ try:
                 duration = end_time - start_time
                 rounded_duration = round(duration, 1)
                 log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                print(log_time + "Recording stopped. Duration:", time_converter(duration))
+                print(log_time + "Recording stopped. Duration:",
+                      time_converter(duration))
                 print(log_time + "Saving the observation...")
                 with open(logger_path, 'a') as file:
-                    print(log_time + "Recording stopped. Duration:", time_converter(duration), file=file)
+                    print(log_time + "Recording stopped. Duration:",
+                          time_converter(duration), file=file)
                     print(log_time + "Saving the observation...", file=file)
-                rounded_dur =round(duration, 1)
+                rounded_dur = round(duration, 1)
                 start_time = 0
                 if colorwriter is not None:
                     colorwriter.release()
@@ -148,7 +162,8 @@ try:
                 if audio_frames:
                     with wave.open(audio_path, 'wb') as audio_file:
                         audio_file.setnchannels(audio_channels)
-                        audio_file.setsampwidth(p.get_sample_size(audio_format))
+                        audio_file.setsampwidth(
+                            p.get_sample_size(audio_format))
                         audio_file.setframerate(audio_sample_rate)
                         audio_file.writeframes(b''.join(audio_frames))
                     audio_frames = []
@@ -157,13 +172,16 @@ try:
                     video_clip = VideoFileClip(color_path)
                     audio_clip = AudioFileClip(audio_path)
                     video_with_audio = video_clip.set_audio(audio_clip)
-                    combined_path = os.path.join(recordings_folder, f'{current_time}_combined.avi')
-                    video_with_audio.write_videofile(combined_path, codec='libx264', logger=None)
+                    combined_path = os.path.join(
+                        recordings_folder, f'{current_time}_combined.avi')
+                    video_with_audio.write_videofile(
+                        combined_path, codec='libx264', logger=None)
                     video_clip.close()
                     audio_clip.close()
 
                 # Save data as BORIS project file
-                project_file_name = projects_folder + "/subject" + str(subject) + ".boris"
+                project_file_name = projects_folder + \
+                    "/subject" + str(subject) + ".boris"
                 events_list_str = json.dumps(events_list)
                 target_string = "}, \"behavioral_categories\""
                 string_to_insert = f'"{current_time}": {{"file": {{"1": ["C:/Data/Recordings/{current_time}_combined.avi"], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": []}}, "type": "MEDIA", "date": "{current_time_prj}", "description": "", "time offset": 0.0, "events": {events_list_str}, "observation time interval": [0, 0], "independent_variables": {{}}, "visualize_spectrogram": false, "visualize_waveform": false, "media_creation_date_as_offset": false, "media_scan_sampling_duration": 0, "image_display_duration": 1, "close_behaviors_between_videos": false, "media_info": {{"length": {{"C:/Users/Wei/Documents/Recordings/{current_time}_combined.avi": {rounded_duration}}}, "fps": {{"C:/Users/Wei/Documents/Recordings/{current_time}_combined.avi": 30.0}}, "hasVideo": {{"C:/Users/Wei/Documents/Recordings/{current_time}_combined.avi": true}}, "hasAudio": {{"C:/Users/Wei/Documents/Recordings/{current_time}_combined.avi": true}}, "offset": {{"1": 0.0}}}}}}'
@@ -176,7 +194,8 @@ try:
                 # Check if the target string was found
                 if index != -1:
                     # Insert the desired string just before the target string
-                    modified_content = content[:index] + string_to_insert + content[index:]
+                    modified_content = content[:index] + \
+                        string_to_insert + content[index:]
                     index = content.find(target_string)
                     # Open the file in write mode and write the modified content back to the file
                     with open(project_file_name, 'w') as file:
@@ -184,93 +203,113 @@ try:
                         log_time = time.strftime("%T-%m/%d/%Y") + ": "
                         print(log_time + "Observation saved successfully.\n")
                         with open(logger_path, 'a') as file:
-                            print(log_time + "Observation saved successfully.\n", file=file)
-                            
+                            print(
+                                log_time + "Observation saved successfully.\n", file=file)
+
             break
-                    
 
         if recording:
             if keyboard.is_pressed('ctrl+alt+a'):
                 time.sleep(0.15)
                 if not b1:
                     b1_start = time.time() - start_time
-                    event = [round(b1_start, 1), "name1", "code1","", "", "NA"]
+                    event = [round(b1_start, 1), "name1",
+                             "code1", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior1 starts at:", time_converter(b1_start))
+                    print(log_time + "Behavior1 starts at:",
+                          time_converter(b1_start))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior1 starts at:", time_converter(b1_start), file=file)
+                        print(log_time + "Behavior1 starts at:",
+                              time_converter(b1_start), file=file)
                     b1 = 1
                 else:
                     b1_stop = time.time() - start_time
-                    event = [round(b1_stop, 1), "name1", "code1","", "", "NA"]
+                    event = [round(b1_stop, 1), "name1", "code1", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior1 ends at:  ", time_converter(b1_stop))
+                    print(log_time + "Behavior1 ends at:  ",
+                          time_converter(b1_stop))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior1 ends at:  ", time_converter(b1_stop), file=file)
+                        print(log_time + "Behavior1 ends at:  ",
+                              time_converter(b1_stop), file=file)
                     b1 = 0
             elif keyboard.is_pressed('ctrl+alt+b'):
                 time.sleep(0.15)
                 if not b2:
                     b2_start = time.time() - start_time
-                    event = [round(b2_start, 1), "name1", "code2","", "", "NA"]
+                    event = [round(b2_start, 1), "name1",
+                             "code2", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior2 starts at:", time_converter(b2_start))
+                    print(log_time + "Behavior2 starts at:",
+                          time_converter(b2_start))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior2 starts at:", time_converter(b2_start), file=file)
+                        print(log_time + "Behavior2 starts at:",
+                              time_converter(b2_start), file=file)
                     b2 = 1
                 else:
                     b2_stop = time.time() - start_time
-                    event = [round(b2_stop, 1), "name1", "code2","", "", "NA"]
+                    event = [round(b2_stop, 1), "name1", "code2", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior2 ends at:  ", time_converter(b2_stop))
+                    print(log_time + "Behavior2 ends at:  ",
+                          time_converter(b2_stop))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior2 ends at:  ", time_converter(b2_stop), file=file)
+                        print(log_time + "Behavior2 ends at:  ",
+                              time_converter(b2_stop), file=file)
                     b2 = 0
             elif keyboard.is_pressed('ctrl+alt+c'):
                 time.sleep(0.15)
                 if not b3:
                     b3_start = time.time() - start_time
-                    event = [round(b3_start, 1), "name1", "code3","", "", "NA"]
+                    event = [round(b3_start, 1), "name1",
+                             "code3", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior3 starts at:", time_converter(b3_start))
+                    print(log_time + "Behavior3 starts at:",
+                          time_converter(b3_start))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior3 starts at:", time_converter(b3_start), file=file)
+                        print(log_time + "Behavior3 starts at:",
+                              time_converter(b3_start), file=file)
                     b3 = 1
                 else:
                     b3_stop = time.time() - start_time
-                    event = [round(b3_stop, 1), "name1", "code3","", "", "NA"]
+                    event = [round(b3_stop, 1), "name1", "code3", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior3 ends at:  ", time_converter(b3_stop))
+                    print(log_time + "Behavior3 ends at:  ",
+                          time_converter(b3_stop))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior3 ends at:  ", time_converter(b3_stop), file=file)
+                        print(log_time + "Behavior3 ends at:  ",
+                              time_converter(b3_stop), file=file)
                     b3 = 0
             elif keyboard.is_pressed('ctrl+alt+d'):
                 time.sleep(0.15)
                 if not b4:
                     b4_start = time.time() - start_time
-                    event = [round(b4_start, 1), "name1", "code4","", "", "NA"]
+                    event = [round(b4_start, 1), "name1",
+                             "code4", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior4 starts at:", time_converter(b4_start))
+                    print(log_time + "Behavior4 starts at:",
+                          time_converter(b4_start))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior4 starts at:", time_converter(b4_start), file=file)
+                        print(log_time + "Behavior4 starts at:",
+                              time_converter(b4_start), file=file)
                     b4 = 1
                 else:
                     b4_stop = time.time() - start_time
-                    event = [round(b4_stop, 1), "name1", "code4","", "", "NA"]
+                    event = [round(b4_stop, 1), "name1", "code4", "", "", "NA"]
                     events_list.append(event)
                     log_time = time.strftime("%T-%m/%d/%Y") + ": "
-                    print(log_time + "Behavior4 ends at:  ", time_converter(b4_stop))
+                    print(log_time + "Behavior4 ends at:  ",
+                          time_converter(b4_stop))
                     with open(logger_path, 'a') as file:
-                        print(log_time + "Behavior4 ends at:  ", time_converter(b4_stop), file=file)
+                        print(log_time + "Behavior4 ends at:  ",
+                              time_converter(b4_stop), file=file)
                     b4 = 0
-            
+
 finally:
     if colorwriter is not None:
         colorwriter.release()
